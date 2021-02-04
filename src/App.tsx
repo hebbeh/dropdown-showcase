ecdeb3f3c3ea8d6cbb610176066ccb49e4249c6a
Anna Päärni <annpaa91@gmail.com>
2021-02-04 20:59:17 +0100

    Trying out the Dropdown.Menu and Dropdown.Item in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,29 @@ import './App.css';
 import Typography from './Components/Style/Typography';
 import Space from './Components/Layout/Space';
 import Divider from './Components/Layout/Divider';
+import Dropdown from './Components/Input/Dropdown';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="headerContainer">
-          <Typography type="header">Dropdown component</Typography>
-          <Space type="medium" />
-          <Divider type="decorative" />
-        </div>
+    <div className="container">
+      <div className="headerContainer">
+        <Typography type="header">Dropdown component</Typography>
+        <Space type="medium" />
+        <Divider type="decorative" />
+        <Dropdown.Menu>
+          <Dropdown.Item>
+            <Typography type="content">Dropdown item</Typography>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <Typography type="content">Dropdown item</Typography>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <Typography type="content">Dropdown item</Typography>
+          </Dropdown.Item>
+          <Dropdown.Item>
+            <Typography type="content">Dropdown item</Typography>
+          </Dropdown.Item>
+        </Dropdown.Menu>
       </div>
     </div>
   );
