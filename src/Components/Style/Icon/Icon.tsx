f1e24f7f885e1aa32d8dd019214f70a69240c858
Anna Päärni <annpaa91@gmail.com>
2021-02-06 14:42:31 +0100

    Updated Icon

--- src/Components/Style/Icon/Icon.tsx
+++ src/Components/Style/Icon/Icon.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './Icon.css';
-import placeholder from './Images/MenuIconPlaceholder.png';
+import styles from './Icon.module.css';
+import menu from './Images/menu.png';
+import placeholder from './Images/placeholder.png';
 
 export interface IconProps {
-  type?: 'placeholder' | 'menu';
+  type: 'placeholder' | 'menu';
 }
 
 export default function Icon({ type }: IconProps) {
-  return <img className={type} alt={type} src={placeholder} />;
+  const imageSrc = { menu: menu, placeholder: placeholder };
+
+  return <img className={styles[type]} alt={type} src={imageSrc[type]} />;
 }
