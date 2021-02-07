ad6f77431ec36aabe4d1033907edb3bde2b8f9e5
Anna Päärni <annpaa91@gmail.com>
2021-02-07 11:03:33 +0100

    Cleaned up DropdownRoute to call two dropdowns

--- src/Routes/DropdownShowcaseRoute.tsx
+++ src/Routes/DropdownShowcaseRoute.tsx
@@ -12,10 +12,6 @@ export default function DropdownShowcaseRoute() {
     'No action selected'
   );
 
-  // The consumer of the dropdown is in charge of what do do when the Option/OptionList onChange function is called
-  // In this simple usecase the states firstMenuAction and secondMenuAction are updated
-  const handleSelectChange = (value: string) => setFirstMenuAction(value);
-
   return (
     <>
       <Typography type="subheader">
@@ -23,38 +19,8 @@ export default function DropdownShowcaseRoute() {
           Selected action in menu: {firstMenuAction}
         </div>
       </Typography>
-      <Space type="medium" />
-      <Dropdown
-        renderTrigger={(onChange: any) => {
-          return (
-            <Button
-              onClick={() => {
-                onChange();
-              }}
-            >
-              <Typography type="content">Menu One</Typography>
-            </Button>
-          );
-        }}
-      >
-        <Dropdown.Menu>
-          <Dropdown.OptionList
-            options={dataToBePutInAMenu}
-            onChange={() => handleSelectChange}
-            renderItem={(onChange: any, option: any) => {
-              return (
-                <Dropdown.Option
-                  onChange={() => {
-                    setFirstMenuAction(option.value);
-                  }}
-                >
-                  <Typography type="content">{option.label}</Typography>
-                </Dropdown.Option>
-              );
-            }}
-          />
-        </Dropdown.Menu>
-      </Dropdown>
+      <Space type="big" />
+      <DropDownExampleOne dropdownAction={setFirstMenuAction} />
       <Space type="big" />
       <Space type="big" />
       <Typography type="subheader">
@@ -63,77 +29,104 @@ export default function DropdownShowcaseRoute() {
         </div>
       </Typography>
       <Space type="medium" />
-      <Dropdown
-        renderTrigger={(onChange: any) => {
-          return (
-            <div
-              onClick={() => {
-                onChange();
-              }}
-            >
-              <Icon type="menu" />
-            </div>
-          );
-        }}
-      >
-        <Dropdown.Menu>
-          <Dropdown.Option
-            onChange={() => {
-              setSecondMenuAction('Action IC 1');
-            }}
-          >
-            <Icon type="placeholder" />
-            <Space type="small" />
-            <Typography type="content">
-              Individually composed option 1
-            </Typography>
-          </Dropdown.Option>
-          <Space type="tiny" />
-          <Divider type="data" />
-          <Space type="tiny" />
-          <Dropdown.Option
-            onChange={() => {
-              setSecondMenuAction('Action IC 2');
-            }}
-          >
-            <Icon type="placeholderII" />
-            <Space type="small" />
-            <Typography type="content">
-              Individually composed option 2
-            </Typography>
-          </Dropdown.Option>
-          <Dropdown.Option
-            onChange={() => {
-              setSecondMenuAction('Action IC 3');
-            }}
-          >
-            <Icon type="placeholderIII" />
-            <Space type="small" />
-            <Typography type="content">
-              Individually composed option 3
-            </Typography>
-          </Dropdown.Option>{' '}
-          <Dropdown.Option
-            onChange={() => {
-              setSecondMenuAction('Action IC 4');
-            }}
-          >
-            <Icon type="placeholderIV" />
-            <Space type="small" />
-            <Typography type="content">
-              Individually composed option 4
-            </Typography>
-          </Dropdown.Option>
-        </Dropdown.Menu>
-      </Dropdown>
+      <DropDownExampleTwo dropdownAction={setSecondMenuAction} />
     </>
   );
 }
 
+interface DropdownExampleProps {
+  dropdownAction(action: string): void;
+}
+
+function DropDownExampleOne({ dropdownAction }: DropdownExampleProps) {
+  return (
+    <Dropdown
+      menuPlacement="bottom-start"
+      renderTrigger={(onClick: any) => {
+        return (
+          <Button onClick={onClick}>
+            <Typography type="content">Menu One</Typography>
+          </Button>
+        );
+      }}
+    >
+      <Dropdown.Menu>
+        {dataToBePutInAMenu.map((option) => {
+          return (
+            <Dropdown.Option
+              onChange={() => {
+                dropdownAction(option.value);
+              }}
+            >
+              <Typography type="content">{option.label}</Typography>
+            </Dropdown.Option>
+          );
+        })}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
 const dataToBePutInAMenu = [
-  { label: 'Label option item 1', value: 'Action1' },
-  { label: 'Label option item 2', value: 'Action2' },
-  { label: 'Label option item 3', value: 'Action3' },
-  { label: 'Label option item 4', value: 'Action4' },
-  { label: 'Label option item 5', value: 'Action5' }
+  { label: 'Label option item 1', value: 'Action 1' },
+  { label: 'Label option item 2', value: 'Action 2' },
+  { label: 'Label option item 3', value: 'Action 3' },
+  { label: 'Label option item 4', value: 'Action 4' },
+  { label: 'Label option item 5', value: 'Action 5' }
 ];
+
+function DropDownExampleTwo({ dropdownAction }: DropdownExampleProps) {
+  return (
+    <Dropdown
+      renderTrigger={(onChange: any) => {
+        return (
+          <div onClick={onChange}>
+            <Icon type="menu" />
+          </div>
+        );
+      }}
+    >
+      <Dropdown.Menu>
+        <Dropdown.Option
+          onChange={() => {
+            dropdownAction('Action 1');
+          }}
+        >
+          <Icon type="placeholder" />
+          <Space type="small" />
+          <Typography type="content">Individually composed option 1</Typography>
+        </Dropdown.Option>
+        <Space type="tiny" />
+        <Divider type="data" />
+        <Space type="tiny" />
+        <Dropdown.Option
+          onChange={() => {
+            dropdownAction('Action 2');
+          }}
+        >
+          <Icon type="placeholderII" />
+          <Space type="small" />
+          <Typography type="content">Individually composed option 2</Typography>
+        </Dropdown.Option>
+        <Dropdown.Option
+          onChange={() => {
+            dropdownAction('Action 3');
+          }}
+        >
+          <Icon type="placeholderIII" />
+          <Space type="small" />
+          <Typography type="content">Individually composed option 3</Typography>
+        </Dropdown.Option>
+        <Dropdown.Option
+          onChange={() => {
+            dropdownAction('Action 4');
+          }}
+        >
+          <Icon type="placeholderIV" />
+          <Space type="small" />
+          <Typography type="content">Individually composed option 4</Typography>
+        </Dropdown.Option>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
