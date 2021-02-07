742a1969c9725647bef8d77a09de3d5c771a3eab
Anna Päärni <annpaa91@gmail.com>
2021-02-07 12:23:02 +0100

    Added a test that renders the Dropdown

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Dropdown from './Components/Input/Dropdown';
+import Typography from './Components/Style/Typography';
+import Button from './Components/Input/Button';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <Dropdown
+      menuPlacement="bottom-start"
+      renderTrigger={(onClick: any) => {
+        return (
+          <Button onClick={onClick}>
+            <Typography type="content">Menu trigger</Typography>
+          </Button>
+        );
+      }}
+    >
+      <Dropdown.Menu>
+        <Dropdown.Option onChange={() => {}}>
+          <Typography type="content">"Label"</Typography>
+        </Dropdown.Option>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+  const triggerElement = screen.getByText(/Menu trigger/i);
+  expect(triggerElement).toBeInTheDocument();
 });
